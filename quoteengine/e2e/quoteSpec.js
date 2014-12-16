772f2bee598b426cdc9d81f40c370b4a244b3cc7
Allen Brubaker <allenbrubaker@gmail.com>
2014-12-16 15:11:33 -0500

    Test to validate compare plans functionality.

--- quoteengine/e2e/quoteSpec.js
+++ quoteengine/e2e/quoteSpec.js
@@ -64,28 +64,6 @@ describe('quote:', function () {
             });
         });
 
-        it('shows plan info modal on plan info button click', function () {
-            quote.plans().spread(function(plan) { 
-                return Promise.props({
-                    name: plan.name(),
-                    premium: plan.premium()
-                })
-                .then(function (result) {
-                    return plan.showPlanInfo().then(function () {
-                        return Promise.props({
-                            name: plan.planInfo.name(),
-                            premium: plan.planInfo.premium()
-                        })
-                        .then(function (info) {
-                            result.name.should.equal(info.name);
-                            result.premium.should.equal(info.premium);
-                        })
-                    })
-                })
-            })
-        });
-        
-        
     });
 
     describe("sort:", function () {
@@ -119,4 +97,39 @@ describe('quote:', function () {
             })
         })
     })
+
+    it('shows plan info modal on plan info button click', function () {
+        quote.plans().spread(function (plan) {
+            return Promise.all([plan, plan.name(), plan.premium()])
+        }).spread(function (plan, name, premium) {
+            return plan.showPlanInfo()
+                .then(function () {
+                    return plan.planInfo.containsPlan(name, premium).should.eventually.be.true;
+                })
+        })
+    });
+
+    it('comparing plans displays details for each plan on compare page', function () {
+        quote.plans()
+            .spread(function (p1, p2) {
+                return Promise.map([p1, p2], function (p) {
+                    return p.clickCompare().then(function () {
+                        return Promise.props({
+                            name: p.name(),
+                            premium: p.premium()
+                        })
+                    })
+                }).then(function (plans) {
+                    return quote.clickComparePlans()
+                        .then(function () {
+                            return plans.reduce(function (acc, plan) {
+                                return quote.comparePage().containsPlan(plan.name, plan.premium)
+                                    .then(function (contains) {
+                                        return acc && contains;
+                                    });
+                            }, true).should.eventually.equal(true);
+                        });
+                })
+            });
+    });
 });
