4e03c0eaab41365541d6371a9703074a9c31f478
Allen Brubaker <allen.brubaker@ritterim.com>
2014-12-17 14:56:39 -0500

    Fixed syntax errors in TaxCredit

--- src/quoteengine/quote/TaxCredit.js
+++ src/quoteengine/quote/TaxCredit.js
@@ -1,44 +1,46 @@
-(function(){
+(function () {
     module.exports = TaxCredit;
-    
-    function TaxCredit(){
+
+    function TaxCredit() {
         var self = this;
         var controls = {
             premiumsWithTaxCredit: controls.all(by.binding('plan.deductiblePrice')),
             taxCredit: controls.element(by.css('.label-success.label-xlb')),
             premiumsWithoutTaxCredit: controls.all(by.css('.oswald.small-premium.slashed'))
         };
-    };
-    
-    self.premiumsWithoutTaxCredit = value(controls.premiumsWithoutTaxCredit);
-    self.taxCredit = function(){
-        return controls.taxCredit.getText().then(function(x){
-            return x.trimCurrency();
-        });
-    }
-    
-    self.premiumsWithTaxCredit = value(controls.premiumsWithTaxCredit);
-    
-    self.isCorrectPremium = function(){
-        Promise.props({
-            creditedPremium: self.premiumsWithTaxCredit(),
-            uncreditedPremium: self.premiumsWithoutTaxCredit(),
-        }).then(function(result){
-            for(int i = 0; i < result.creditedPremium.length; i++){
-                return taxCredit == result.uncreditedPremium[i] - result.creditedPremium[i];
-            }
-        });
-    }
-    
-    var value = function(item){
-        return function(){
-            return Promise.resolve(item
-                .map(function(control){
-                    return control.getText().then(function(x){
-                        return x.trimCurrency();
-                    });
-                }));
+
+        self.premiumsWithoutTaxCredit = value(controls.premiumsWithoutTaxCredit);
+        self.taxCredit = function () {
+            return controls.taxCredit.getText().then(function (x) {
+                return x.trimCurrency();
+            });
         }
-    }
-    
+
+        self.premiumsWithTaxCredit = value(controls.premiumsWithTaxCredit);
+
+        self.isCorrectPremium = function () {
+            Promise.props({
+                creditedPremium: self.premiumsWithTaxCredit(),
+                uncreditedPremium: self.premiumsWithoutTaxCredit(),
+            }).then(function (result) {
+                for (var i = 0; i < result.creditedPremium.length; i++) {
+                    return taxCredit == result.uncreditedPremium[i] - result.creditedPremium[i];
+                }
+            });
+        }
+
+        var value = function (item) {
+            return function () {
+                return Promise.resolve(item
+                    .map(function (control) {
+                        return control.getText().then(function (x) {
+                            return x.trimCurrency();
+                        });
+                    }));
+            }
+        }
+    };
+
+
+
 })();
